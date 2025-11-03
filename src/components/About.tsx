9ae91ec8738c28547740ac4a1931f081836caa4f
Raihana0711 <raihanaa.nun07114@gmail.com>
2025-11-03 08:28:22 +0800

    Update About.tsx

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -3,16 +3,14 @@ export default function About() {
 		<section id="about" className="space-y-6">
 			<h3 className="text-sm uppercase tracking-wider font-medium text-zinc-500 dark:text-zinc-300">About</h3>
 			<p className="text-zinc-800 dark:text-zinc-200">
-				Ruben Amorim adalah pelatih sepak bola asal Portugal yang kini menukangi Manchester United.
-				Dikenal dengan filosofi permainan menyerang dan pressing intensitas tinggi, Amorim membawa pendekatan taktik modern
-				yang berakar dari pengalamannya bersama Sporting CP. Ia menekankan kerja sama tim, disiplin posisi,
-				serta pengembangan pemain muda dalam setiap proyeknya.
+				Raihana adalah seorang mahasiswa S1 Teknik Informatika di Sekolah Tinggi Teknologi Bontang yang memiliki ketertarikan besar terhadap dunia teknologi modern, khususnya pada bidang pengembangan perangkat lunak, desain sistem informasi, dan kecerdasan buatan. Sejak awal kuliah, Raihana aktif mendalami pemrograman dan teknologi digital melalui berbagai tugas praktikum, proyek akademik, serta eksplorasi mandiri di luar kelas.
+
+Sebagai pribadi yang antusias terhadap inovasi, Raihana percaya bahwa teknologi bukan hanya alat, tetapi juga solusi masa depan untuk berbagai tantangan di masyarakat. Dengan kemampuan analisis yang kuat dan semangat belajar berkelanjutan, Raihana berfokus membangun keterampilan teknis seperti pengembangan web, pemrograman berbasis proyek, manajemen database, serta dasar-dasar machine learning.
+
 			</p>
 			<p className="text-zinc-800 dark:text-zinc-200">
-				Saat ini, Amorim fokus membangun kembali identitas permainan Manchester United dengan gaya dinamis,
-				cepat dalam transisi, dan berorientasi pada penguasaan bola. Tujuannya jelas mengembalikan kejayaan klub
-				di panggung domestik maupun Eropa.
-			</p>
+							Dalam setiap proses pembelajaran, Raihana menekankan disiplin, kerja sama tim, dan kreativitas sebagai fondasi kesuksesan. Selain itu, Raihana juga memiliki minat dalam pengembangan UI/UX, memastikan solusi tidak hanya berfungsi dengan baik tetapi juga mudah digunakan dan memiliki pengalaman pengguna yang baik.
+							Ke depan, Raihana bercita-cita untuk berkontribusi secara nyata di dunia teknologi melalui pembuatan aplikasi dan sistem cerdas yang dapat membantu kebutuhan masyarakat, mendukung transformasi digital, dan menghadirkan inovasi berdampak luas. Dengan komitmen kuat serta dedikasi yang tinggi, Raihana terus mempersiapkan diri untuk menjadi bagian dari talenta teknologi yang unggul dan kompetitif.			</p>
 		</section>
 	);
 }
