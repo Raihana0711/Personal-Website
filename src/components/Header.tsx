6fe283d9a54339148d922a93d83362ab832dfe14
Raihana0711 <raihanaa.nun07114@gmail.com>
2025-11-03 08:30:33 +0800

    Update Header.tsx

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -2,24 +2,24 @@ import Image from "next/image";
 
 export default function Header() {
   return (
-    <header className=" flex gap-x-8 flex-col md:flex-row">
+    <header className="flex gap-x-8 flex-col md:flex-row items-center md:items-start">
       <Image
-        className="size-[100px] rounded-full"
-        src="/amorim.jpg"
+        className="size-[140px] rounded-full"
+        src="/Raihanaa.jpg"
         alt="picture of author"
-        width={100}
-        height={100}
+        width={140}
+        height={140}
       />
       <div>
         <h1 className="text-3xl font-medium text-zinc-900 dark:text-zinc-50 mb-1.5">
-          Ruben Amorim
+          Raihana
         </h1>
         <h2 className="text-lg font-light text-zinc-500 dark:text-zinc-300">
-          Pelatih Manchester United dan mantan pemain sepak bola
+          Mahasiswa Semester 5 Teknik Informatika
         </h2>
         <div className="flex gap-x-6 mt-6 flex-col md:flex-row gap-y-2">
           <span className="text-zinc-500 dark:text-zinc-300">
-            Manchester United FC
+            Sekolah Tinggi Teknologi Bontang
           </span>
         </div>
       </div>
